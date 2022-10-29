0a66e9a3e400b578502555b2c5df75c08c6343bd
Vigneshwaran Elangovan <vygneshwaran@gmail.com>
2022-10-30 00:07:30 +0530

    tabbar 1 initial

--- app/src/main/java/com/vickycodes/tabbar/MainActivity.kt
+++ app/src/main/java/com/vickycodes/tabbar/MainActivity.kt
@@ -4,23 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.DrawableRes
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.clipToBounds
 import androidx.compose.ui.draw.shadow
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.vickycodes.tabbar.ui.theme.TabbarTheme
@@ -45,23 +43,59 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun TabBarView1() {
+    var tabState by remember {
+        mutableStateOf(TabBar.Home)
+    }
     Box(modifier = Modifier
         .padding(start = 24.dp, end = 24.dp)
         .fillMaxWidth()
-        .height(60.dp)
+        .wrapContentHeight()
         .shadow(elevation = 16.dp)
         .clip(RoundedCornerShape(16.dp))
         .background(MaterialTheme.colors.surface)
 
     ) {
-        Row(modifier = Modifier.matchParentSize(),
-            horizontalArrangement = Arrangement.SpaceEvenly,
-            verticalAlignment = Alignment.CenterVertically,) {
+        Box() {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(60.dp),
+                horizontalArrangement = Arrangement.SpaceEvenly,
+                verticalAlignment = Alignment.CenterVertically,
+            ) {
 
-            TabBar1Image(R.drawable.ic_baseline_home_24)
-            TabBar1Image(R.drawable.ic_baseline_videogame_asset_24)
-            TabBar1Image(R.drawable.ic_baseline_layers_24)
-            TabBar1Image(R.drawable.ic_baseline_ondemand_video_24)
+                TabBar1Image(
+                    res = R.drawable.ic_baseline_home_24,
+                    type = TabBar.Home,
+                    isSelected = tabState == TabBar.Home,
+                    onClick = {
+                        tabState = it
+                    })
+                TabBar1Image(
+                    res = R.drawable.ic_baseline_videogame_asset_24,
+                    type = TabBar.Game,
+                    isSelected = tabState == TabBar.Game,
+                    onClick = {
+                        tabState = it
+                    })
+                TabBar1Image(
+                    res = R.drawable.ic_baseline_layers_24,
+                    type = TabBar.Screen,
+                    isSelected = tabState == TabBar.Screen,
+                    onClick = {
+                        tabState = it
+                    })
+                TabBar1Image(
+                    res = R.drawable.ic_baseline_ondemand_video_24,
+                    type = TabBar.Video,
+                    isSelected = tabState == TabBar.Video,
+                    onClick = {
+                        tabState = it
+                    })
+
+            }
+
+            TabCircleInflated()
 
         }
     }
@@ -71,18 +105,53 @@ fun TabBarView1() {
 fun TabBar1Image(
     @DrawableRes res: Int,
     isInflated: Boolean = false,
+    isSelected: Boolean = false,
+    type: TabBar = TabBar.Home,
+    onClick: (type: TabBar) -> Unit = {},
     modifier: Modifier = Modifier,
 ) {
-    Icon(modifier = modifier.padding(16.dp),
+    Icon(modifier = modifier
+        .alpha(if (isSelected) 0f else 1f)
+        .clickable {
+            onClick(type)
+        },
         painter = painterResource(id = res),
         tint = if (isInflated) MaterialTheme.colors.onSecondary else MaterialTheme.colors.onPrimary,
         contentDescription = "tab")
+
 }
 
 @Composable
 fun TabCircleInflated() {
-
+    Box(modifier = Modifier
+        .size(50.dp)
+        .clip(CircleShape)
+        .background(MaterialTheme.colors.background)
+        .padding(4.dp)) {
+        Column(modifier = Modifier
+            .fillMaxSize()
+            .clip(CircleShape)
+            .background(MaterialTheme.colors.onSurface),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            TabBar1Image(R.drawable.ic_baseline_home_24, isInflated = true)
+        }
+    }
 }
+
+enum class TabBar {
+    Home, Game, Screen, Video
+}
+
+@Preview(showBackground = true)
+@Composable
+fun TabCircleInflatedPreview() {
+    TabbarTheme(darkTheme = false) {
+        TabCircleInflated()
+    }
+}
+
 
 @Preview(showBackground = true)
 @Composable
